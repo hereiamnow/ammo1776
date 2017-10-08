15a19182f3ff045f0512eab807074a6e73d58c04
Shawn Miller <hereiamnow@gmail.com>
2017-10-08 15:00:20 -0500

    Initial load of assets

---
+++ assets/scripts/foobar.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Shawn Miller on 10/1/2017
+ * Filename: foobar.js
+ * Path: /
+ * Version: 1.0
+ * Description: A brief description of the Script.
+ */
+
+
+$(document).ready(function () {
+	$('#characterLeft').text('140 characters left');
+	$('#message').keydown(function () {
+		var max = 140;
+		var len = $(this).val().length;
+		if (len >= max) {
+			$('#characterLeft').text('You have reached the limit');
+			$('#characterLeft').addClass('red');
+			$('#btnSubmit').addClass('disabled');
+		}
+		else {
+			var ch = max - len;
+			$('#characterLeft').text(ch + ' characters left');
+			$('#btnSubmit').removeClass('disabled');
+			$('#characterLeft').removeClass('red');
+		}
+	});
+});
